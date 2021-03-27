3d68041fb7a34a20a407bd128af4cb27ce0ef49e
nmisustin <nmisustin@gmail.com>
2021-03-26 21:04:25 -0700

    added 5 day forecast display

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -5,6 +5,11 @@ var farenheit = " &#8457;"
 var todaysDate = moment().format("MMMM Do YYYY");
 var currentCityLocation = {lat:"", lon:""}
 var currentData = {temperature: "", humidity:"", windSpeed: "", uvIndex: ""}
+var fiveDayForecast = [document.getElementById("five-day-1"), 
+    document.getElementById("five-day-2"), 
+    document.getElementById("five-day-3"), 
+    document.getElementById("five-day-4"), 
+    document.getElementById("five-day-5")];
 // http://openweathermap.org/img/wn/10d.png
 function getCurrentCityData(city){
     var apiurl = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=9f51e82ea4028359e9b2d5a85c069491&units=imperial"
@@ -60,6 +65,25 @@ function getCurrentCityData(city){
                             var uvIndexEl = document.createElement("p")
                             uvIndexEl.innerHTML="UV Index: <span class = '" + uvColor +"'>"+uvIndex+"</span>"
                             currentWeatherEl.appendChild(uvIndexEl)
+                            for (var i =0; i < fiveDayForecast.length; i++){
+                                var date = moment().add(1+i, "days");
+                                date = date.format("M/D/YY");
+                                dateEl=document.createElement("p")
+                                dateEl.innerHTML = date;
+                                fiveDayForecast[i].appendChild(dateEl);
+                                var weatherSymbol = "http://openweathermap.org/img/wn/"+ data.daily[i].weather[0].icon +".png"
+                                var weatherSymbolEl = document.createElement("img");
+                                weatherSymbolEl.setAttribute("src", weatherSymbol);
+                                fiveDayForecast[i].appendChild(weatherSymbolEl);
+                                var temp = data.daily[i].temp.day;
+                                var tempEl = document.createElement("p")
+                                tempEl.innerHTML= "Temperature: " + temp + farenheit;
+                                fiveDayForecast[i].appendChild(tempEl);
+                                var humidity = data.daily[i].humidity;
+                                var humidityEl = document.createElement("p");
+                                humidityEl.innerHTML = "Humidity: "+humidity+"%";
+                                fiveDayForecast[i].appendChild(humidityEl);
+                            }
                         })
                     }
                     else{
